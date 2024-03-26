639a54fd192b1a8db4459518dc54ba4d90330b3c
STEVEN BACHIMONT <stevenbachimont@gmail.com>
2024-03-26 11:13:13 +0100

    Bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+
+const PokemonCard = () => {
+    return (
+        <div className="pokemon-card">
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+        </div>
+    );
+};
+
+export default PokemonCard;
