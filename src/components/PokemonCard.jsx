b70c9e592b6eb28932ee7b3e8ad9f7aedf4031cb
STEVEN BACHIMONT <stevenbachimont@gmail.com>
2024-03-26 12:11:38 +0100

    les explications de cette quête sont assez difficile, on est pas encore expert git status

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,25 @@
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 const PokemonCard = () => {
+    let pokemon = pokemonList[1];
     return (
         <div className="pokemon-card">
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-                <figcaption>Bulbasaur</figcaption>
+                {pokemon.imgSrc ? (
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p>???</p>
+                )}
+                <figcaption>{pokemon.name}</figcaption>
             </figure>
         </div>
     );
