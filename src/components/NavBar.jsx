c657579a7f08ab172bfb5c59c8961c4f6cebf2c6
STEVEN BACHIMONT <stevenbachimont@gmail.com>
2024-03-29 11:42:36 +0100

    quête8

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,19 @@
 import PropTypes from "prop-types";
 
-function NavBar({ onPrevious, onNext }) {
+function NavBar({ onPrevious, onNext, pokemonList, onPokemonClick }) {
+
+
+    const selectClick = (index) => {
+        onPokemonClick(index);
+    };
+
     return (
         <div className="buttons">
             <button onClick={onPrevious} className="moins1">Précédent</button>
+
+            {pokemonList.map((pokemon, index) => (
+                <button key={index} onClick={() => selectClick(index)}>{pokemon.name}</button>
+            ))}
             <button onClick={onNext} className="plus1">Suivant</button>
         </div>
     );
@@ -12,6 +22,8 @@ function NavBar({ onPrevious, onNext }) {
 NavBar.propTypes = {
     onPrevious: PropTypes.func.isRequired,
     onNext: PropTypes.func.isRequired,
-}
+    pokemonList: PropTypes.array.isRequired,
+    onPokemonClick: PropTypes.func.isRequired,
+};
 
 export default NavBar;
